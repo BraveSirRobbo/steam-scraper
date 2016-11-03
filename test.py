2120fb09b289375d624d8bf8da4c5257bd30f305
RobBrenn <me@example.com>
2016-11-03 11:50:14 +0000

    Read example wen html as part of test

--- test.py
+++ test.py
@@ -5,7 +5,11 @@ def scrapePage(html):
 
 class ScraperTests(unittest.TestCase):
     def test_example_page(self):
-        self.assertEqual(scrapePage(""),
+
+	with open("examples/Age of Wonders III on Steam.html", "r") as f:
+		page_text = "".join(f.readlines())
+
+        self.assertEqual(scrapePage(page_text),
 		{ "title" : "Age of Wonders III"
 		, "overall_rating" : "Very Positive"
 		, "num_reviews" : "3505"
