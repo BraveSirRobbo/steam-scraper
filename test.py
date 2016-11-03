4ecee066296e8f463220c7705da910cec010ae8a
James Williams <j@Jamess-MacBook-2.local>
2016-11-03 13:43:34 +0000

    Refactored test_example_age test to test keys seperately

--- test.py
+++ test.py
@@ -16,27 +16,41 @@ def scrapePage(html_doc):
                 { "class": re.compile(r"game_review_summary .*")})[1]).get_text()
 		      , "num_reviews" :
             justOne(soup.find_all("meta",attrs={"itemprop":"reviewCount"})).attrs["content"]
+          , "release_year" : ""
+          , "user_tags" : ""
+          , "Multiplayer" : ""
+          , "Co-op" : ""
+          , "local_multiplayer" : ""
+          , "online_multiplayer" : ""
+          , "steam_cloud" : ""
+          , "controller_supported" : ""
 		      }
 
+
 class ScraperTests(unittest.TestCase):
+    def assertKeyValue(self, d, key, value):
+      self.assertIn(key, d)
+      self.assertEqual(d[key], value)
+
     def test_example_page(self):
 
-	with open("examples/Age of Wonders III on Steam.html", "r") as f:
-		page_text = "".join(f.readlines())
+      with open("examples/Age of Wonders III on Steam.html", "r") as f:
+        page_text = "".join(f.readlines())
 
-        self.assertEqual(scrapePage(page_text),
-		{ "title" : "Age of Wonders III"
-		, "overall_rating" : "Very Positive"
-		, "num_reviews" : "3504"
-		, "release_year" : "2014" #from class "release_date"
-		, "user_tags" : "Strategy, Turn-Based Strategy, Fantasy, RPG" #from class "glance_tags popular_tags"
-		, "Multiplayer" : "True" #"Multi-Player" from class "game_area_details_specs"
-		, "Co-op" : "True" #"Co-op" from class "game_area_details_specs"
-		, "local_multiplayer" : "True" #"Shared/Split Screen" from class "game_area_details_specs"
-		, "online_multiplayer" : "True" #from class "game_area_details_specs"
-		, "steam_cloud" : "True" #Cross-Platform Multiplayer from class "game_area_details_specs"
-		, "controller_supported" : "False" #Full OR Partial Controller Support from class "game_area_details_specs"
-		})
+      res = scrapePage(page_text)
+
+      self.assertKeyValue(res, "title", "Age of Wonders III")
+
+      self.assertKeyValue(res, "overall_rating", "Very Positive")
+      self.assertKeyValue(res, "num_reviews", "3504")
+      self.assertKeyValue(res, "release_year","2014") #from class "release_date"
+      self.assertKeyValue(res, "user_tags", "Strategy, Turn-Based Strategy, Fantasy, RPG") #from class "glance_tags popular_tags"
+      self.assertKeyValue(res, "Multiplayer", "True") #"Multi-Player" from class "game_area_details_specs"
+      self.assertKeyValue(res, "Co-op", "True") #"Co-op" from class "game_area_details_specs"
+      self.assertKeyValue(res, "local_multiplayer", "True") #"Shared/Split Screen" from class "game_area_details_specs"
+      self.assertKeyValue(res, "online_multiplayer", "True") #from class "game_area_details_specs"
+      self.assertKeyValue(res, "steam_cloud", "True") #Cross-Platform Multiplayer from class "game_area_details_specs"
+      self.assertKeyValue(res, "controller_supported", "False") #Full OR Partial Controller Support from class "game_area_details_specs"
 
 if __name__ =="__main__":
 	unittest.main()
